df8ddde15be7d565b48b3d633558919d25d2018f
Matildamathebula <161756130+Matildamathebula@users.noreply.github.com>
2025-09-08 23:34:11 +0200

    added api and axios

--- docs.js
+++ docs.js
@@ -1,12 +1,23 @@
+function displayQuotes(response){
+  
+  new Typewriter("#quote", {
+    strings: response.data.quote,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
 function generateQuote(event){
     event.preventDefault();
+    let instructionsInput=document.querySelector("#user-instructions");
+    let apiKey = "36029o83t7fb6b04f2aa5c604a48d52f";
+    let prompt=`Generate a self-love quote based on the following instructions: ${instructionsInput.value}. The quote should be between 4 and 6 lines.`;
+    let context="You are a helpful assistant that generates self-love quotes, please generate a self-love quote based on the user's instructions. Make sure the quote is between 4 and 6 lines. And it is motivoting to the reader.";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-new Typewriter("#quote", {
-  strings: "To love oneself is the beginning of a lifelong romance.",
-  autoStart: true,
-  delay:1,
-  cursor:"",
-});
+    axios.get(apiUrl).then(displayQuotes);
+
+
 }
 
       let quoteFormElement=document.querySelector("#quote-generator-form");
